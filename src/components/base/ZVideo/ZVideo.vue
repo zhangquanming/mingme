febd97d3a056c495eb566570adcee7ffa4cf17ec
zhangquanming <419654548@qq.com>
2020-07-30 10:55:54 +0800

    1

--- src/components/base/ZVideo/ZVideo.vue
+++ src/components/base/ZVideo/ZVideo.vue
@@ -8,15 +8,42 @@
         <icon class="z-video-box-mask-icon" type="iconplaycircle" :size="50" color="fff" />
       </div>
     </div>
+    <div :class="`z-video-control-wrap ${isShowControl || !isPlaying ? 'z-video-control-show' : 'z-video-control-hide'}`">
+      <div class="z-video-control">
+        <div class="z-video-control-play">
+          <icon :type="isPlaying ? 'iconpause' : 'iconplay'" :size="28" @click="handleTogglePlay" />
+        </div>
+        <div class="z-video-control-progress">
+          <z-progress :value="progressValue" height="100%" @on-click="handleChangeProgress" />
+        </div>
+        <div class="z-video-control-time">{{ videoCurrentTime | formatTime }}</div>
+        <div class="z-video-control-duration">{{ videoDuration | formatTime }}</div>
+        <div class="z-video-control-sound">
+          <icon :type="isMute || !videoVolume ? 'iconmute' : 'iconsound'" :size="20" @click="handleToggleMute" />
+        </div>
+        <div class="z-video-control-volume">
+          <z-progress :value="videoVolume" height="10px" @on-click="handleChangeVolume" />
+        </div>
+        <div class="z-video-control-screen">
+          <icon :type="isFullScreen ? 'iconexitfullscreen-rectangle' : 'iconfullscreen'" :size="20" @click="handleToggleScreen" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Icon from '@/components/base/Icon/Icon'
+import ZProgress from '@/components/base/ZProgress/ZProgress'
+
+let timer = null
+let timerClick = null
+
 export default {
   name: 'ZVideo',
   components: {
-    Icon
+    Icon,
+    ZProgress
   },
   props: {
     src: {
@@ -31,10 +58,54 @@ export default {
   data () {
     return {
       isReadyPlay: false,
-      isPlaying: false
+      isPlaying: false,
+      isMute: false,
+      isFullScreen: false,
+      isShowControl: true,
+      videoVolume: 0,
+      videoVolumeOld: 0,
+      videoCurrentTime: 0,
+      videoDuration: 1,
+      currentTimeFormat: '00:00',
+      durationFormat: '00:00'
     }
   },
-  computed: {},
+  computed: {
+    progressValue () {
+      return this.videoCurrentTime / this.videoDuration
+    }
+  },
+  filters: {
+    formatTime (seconds) {
+      var h = parseInt(seconds / 3600)
+      var m = parseInt((seconds % 3600) / 60)
+      var s = parseInt(seconds % 60)
+      if (h) {
+        return formatZero(h) + ':' + formatZero(m) + ':' + formatZero(s)
+      } else {
+        return formatZero(m) + ':' + formatZero(s)
+      }
+      function formatZero (num) {
+        return num < 10 ? '0' + num : num
+      }
+    }
+  },
+  mounted () {
+    const that = this
+    this.videoPlayer = document.getElementById('videoPlayer')
+    this.videoPlayer.onloadedmetadata = function () {
+      that.videoDuration = this.duration
+      this.volume = that.videoVolume
+      that.isReadyPlay = true
+    }
+    this.videoPlayer.ontimeupdate = function () {
+      const videoCurrentTime = this.currentTime
+      that.videoCurrentTime = videoCurrentTime
+      if (videoCurrentTime === that.videoDuration) {
+        that.isPlaying = false
+      }
+    }
+  },
   watch: {
     isPlaying (value) {
       if (value) {
@@ -42,31 +113,157 @@ export default {
       } else {
         this.videoPlayer.pause()
       }
+    },
+    isMute (value) {
+      if (value) {
+        this.videoPlayer.volume = 0
+        this.videoVolume = 0
+      } else {
+        this.videoPlayer.volume = this.videoVolumeOld
+        this.videoVolume = this.videoVolumeOld
+      }
+    },
+    isFullScreen (value) {
+      if (value) {
+        this.launchFullscreen()
+      } else {
+        this.exitFullscreen()
+      }
     }
   },
-  created () { },
-  mounted () { },
   methods: {
-    /**
-     * @desc mousemove
-     */
-    handleMouseMove () { },
-    /**
-     * @desc mouseleave
-     */
-    handleMouseLeave () { },
     /**
      * @desc 单击 video
      */
-    handleClickVideo () { },
+    handleClickVideo () {
+      clearTimeout(timerClick)
+      timerClick = setTimeout(() => {
+        this.handleTogglePlay()
+      }, 300)
+    },
+
     /**
      * @desc 双击 video
      */
-    handleDbClickVideo () { },
+    handleDbClickVideo () {
+      clearTimeout(timerClick)
+      this.handleToggleScreen()
+    },
+
+    /**
+     * @desc mousemove
+     */
+    handleMouseMove () {
+      this.isShowControl = true
+      if (this.isFullScreen) {
+        this.handleDebounceIsShowControl()
+      }
+    },
+
+    /**
+     * @desc mousemove 防抖事件
+     */
+    handleDebounceIsShowControl () {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        this.isShowControl = false
+      }, 2000)
+    },
+
+    /**
+     * @desc mouseleave
+     */
+    handleMouseLeave () {
+      this.isShowControl = false
+    },
+
+    /**
+     * @desc 进度条点击事件
+     */
+    handleChangeProgress (value) {
+      this.videoPlayer.currentTime = this.videoDuration * value
+    },
+
+    /**
+     * @desc 音量条点击事件
+     */
+    handleChangeVolume (value) {
+      this.videoVolume = value
+      this.videoVolumeOld = value
+      this.videoPlayer.volume = value
+      this.isMute = value <= 0
+    },
     /**
      * @desc 切换播放
      */
-    handleTogglePlay () { }
+    handleTogglePlay () {
+      this.isPlaying = !this.isPlaying
+    },
+
+    /**
+     * @desc 切换静音
+     */
+    handleToggleMute () {
+      this.isMute = !this.isMute
+    },
+
+    /**
+     * @desc 切换全屏
+     */
+    handleToggleScreen () {
+      this.isFullScreen = !this.isFullScreen
+    },
+
+    /**
+     * @desc 进入全屏
+     */
+    launchFullscreen () {
+      const element = this.$refs.ZVideo
+      // 此方法不可以在異步任務中執行，否則火狐無法全屏
+      if (element.requestFullscreen) {
+        element.requestFullscreen()
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen()
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen()
+      } else if (element.oRequestFullscreen) {
+        element.oRequestFullscreen()
+      } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullScreen()
+      } else {
+        var cssText = 'width:100%;height:100%;overflow:hidden;'
+        var docHtml = document.documentElement
+        var docBody = document.body
+        docHtml.style.cssText = cssText
+        docBody.style.cssText = cssText
+      }
+      element.style.cssText = 'margin:0px;padding:0px;position: fixed; top: 0; left: 0; bottom: 0; right: 0; z-index: 999;'
+    },
+
+    /**
+     * @desc 退出全屏
+     */
+    exitFullscreen () {
+      const element = this.$refs.ZVideo
+
+      if (document.exitFullscreen) {
+        document.exitFullscreen()
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen()
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen()
+      } else if (document.oRequestFullscreen) {
+        document.oCancelFullScreen()
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen()
+      } else {
+        var docHtml = document.documentElement
+        var docBody = document.body
+        docHtml.style.cssText = ''
+        docBody.style.cssText = ''
+      }
+      element.style.cssText = ''
+    }
   }
 }
 </script>
