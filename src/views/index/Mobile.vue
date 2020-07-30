93c57dc9c082d3885428ce88834f3d1dae264d27
zhangquanming <419654548@qq.com>
2020-07-30 11:08:53 +0800

    1

---
+++ src/views/index/Mobile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>手机端</div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () { },
+  mounted () { },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
