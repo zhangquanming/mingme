48a4f4964dc956349d7dc9955ce53ac08e1381b3
zhangquanming <419654548@qq.com>
2020-07-31 18:38:07 +0800

    创建管理模块

---
+++ src/views/admin/AdminArticle/AdminArticle.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'AdminArticle',
+  components: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () { },
+  mounted () { },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+</style>
