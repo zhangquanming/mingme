c92d48e1cc6ba765a28e03fe90f9851f4f418590
zhangquanming <419654548@qq.com>
2021-06-05 00:02:04 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-06-04 23:56:26
+ * @LastEditTime: 2021-06-05 00:01:50
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /mingme/src/App.vue
+-->
 <template>
   <div id="app">
     <router-view />
